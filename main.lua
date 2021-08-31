dbb0cdb166ab7b0a2afea9723013534ee8b67a27
HTV04 <53527582+HTV04@users.noreply.github.com>
2021-08-31 00:10:23 -0400

    push v1.0

--- main.lua
+++ main.lua
@@ -1,40 +1,40 @@
-io.stdout:setvbuf('no')
-
 push = require "push" --require the library
-love.window.setTitle("Press space to switch examples")
+love.window.setTitle("Press space to switch examples!")
 
 local examples = {
-  "low-res",
-  "single-shader",
-  "multiple-shaders",
-  "mouse-input",
-  "canvases-shaders",
-  "stencil"
+	"low-res",
+	--[[
+	"single-shader",
+	"multiple-shaders",
+	--]]
+	"mouse-input"
+	--[[
+	"canvases-shaders",
+	"stencil"
+	--]]
 }
 local example = 1
 
 for i = 1, #examples do
-  examples[i] = require("examples." .. examples[i])
+	examples[i] = require("examples." .. examples[i])
 end
 
 function love.resize(w, h)
-  push:resize(w, h)
+	push.resize(w, h)
 end
 
-function love.keypressed(key, scancode, isrepeat)
-  
-  if key == "space" then
-    example = (example < #examples) and example + 1 or 1
-    
-    --be sure to reset push settings
-    push:resetSettings()
-    
-    examples[example]()
-    love.load()
-  elseif key == "f" then --activate fullscreen mode
-    push:switchFullscreen() --optional width and height parameters for window mode
-  end
-  
+function love.keypressed(key)
+	if key == "space" then
+		example = (example < #examples) and example + 1 or 1
+
+		examples[example]()
+		love.load()
+	elseif key == "f" then -- Activate fullscreen mode
+		love.window.setMode(0, 0, {fullscreen = true, fullscreentype = "desktop"})
+		push.resize(love.graphics.getDimensions())
+	elseif key == "escape" then
+		love.event.quit()
+	end
 end
 
 examples[example]()
